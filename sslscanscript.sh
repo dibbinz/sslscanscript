774d3d01eb2acb04a902a0569a3124ab4b9a79fc
dibbinz <155188559+dibbinz@users.noreply.github.com>
2024-09-13 10:57:13 -0400

    Update sslscanscript.sh

--- sslscanscript.sh
+++ sslscanscript.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Constants
-TARGET_IP="<IP>"
-PORTS=(choose ports example: 443 80 587)  # List of ports to scan
+TARGET_IP="<ip>"
+PORTS=(<choose ports example: 443 80 587>)  # List of ports to scan
 
-# Color definitions
+# Color 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m' # <-- No Color
 
 echo -e "${CYAN}Starting SSL scan on $TARGET_IP...${NC}"
 
 for PORT in "${PORTS[@]}"; do
   echo -e "${YELLOW}Scanning $TARGET_IP:$PORT...${NC}"
   
-  # Run the sslscan and capture output
+  # Run the sslscan and get output
   sslscan --no-failed "$TARGET_IP:$PORT"
   
   # Check if the sslscan command was successful
